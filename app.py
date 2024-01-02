1bc66ed99f30b05061d6c50630b72f0e25ad87ab
Ali Sal <68872543+alisal97@users.noreply.github.com>
2024-01-02 10:45:53 +0100

    can handle multiple URLs now

--- app.py
+++ app.py
@@ -3,31 +3,32 @@ from MODULES.scraper import finder, get_content
 import logging
 import config 
 
-app = Flask(__name__)
+app = Flask (__name__)
 
 logging.basicConfig(filename='flask_app.log', filemode='w', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 @app.route('/scrape', methods=['POST'])
 def scrape():
     data = request.json
-    if not data or 'url' not in data:
-        return jsonify({'error': 'No URL provided'}), 400
+    if not data or 'urls' not in data:
+        return jsonify({'error': 'No URLs provided'}), 400
 
-    url = data['url']
-    links = finder(url)
+    results = []
+    for url in data['urls']:
+        links = finder(url)
 
-    if links and not "Contact page not found." in links:
-        results = []
-        for link in links:
-            content = get_content(link)
-            if content:
-                results.append({
-                    'url': link,
-                    'content': content
-                })
-        return jsonify(results), 200
-    else:
-        return jsonify({'error': 'Relevant pages not found.'}), 404
+        if links and not "Contact page not found." in links:
+            for link in links:
+                content = get_content(link)
+                if content:
+                    results.append({
+                        'url': link,
+                        'content': content
+                    })
+        else:
+            return jsonify({'error': f'Relevant pages not found for URL: {url}'}), 404
+
+    return jsonify(results), 200
 
 if __name__ == '__main__':
     app.run(debug=True, port=config.port)
